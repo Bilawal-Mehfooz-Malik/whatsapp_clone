8717b6516a73f6f772581103f9f4fe0ee49a7c59
Bilawal-Mehfooz-Malik <bilawalmehfoozflutter@gmail.com>
2023-12-05 23:02:02 +0500

    added image picker and other features

--- lib/features/auth/screens/otp_screen.dart
+++ lib/features/auth/screens/otp_screen.dart
@@ -28,55 +28,57 @@ class OtpScreen extends ConsumerWidget {
     final textColor = lightTheme ? lightTabColor : tabColor;
     final bgColor = lightTheme ? whiteColor : backgroundColor;
     final size = MediaQuery.of(context).size;
-    return Scaffold(
-      appBar: AppBar(
-        elevation: 0,
-        centerTitle: true,
-        backgroundColor: bgColor,
-        foregroundColor: textColor,
-        title: Text('Verify $phoneNumber'),
-      ),
-      body: Center(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            const SizedBox(height: 20),
-            const Text('We have sent an SMS with a code.'),
+    return SafeArea(
+      child: Scaffold(
+        appBar: AppBar(
+          elevation: 0,
+          centerTitle: true,
+          backgroundColor: bgColor,
+          foregroundColor: textColor,
+          title: Text('Verify $phoneNumber'),
+        ),
+        body: Center(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              const SizedBox(height: 20),
+              const Text('We have sent an SMS with a code.'),
 
-            // Otp input field
-            SizedBox(
-              width: size.width * 0.35,
-              child: TextField(
-                cursorColor: textColor,
-                textAlign: TextAlign.center,
-                style: const TextStyle(fontSize: 20),
-                keyboardType: TextInputType.number,
-                decoration: const InputDecoration(
-                  hintText: '- - - - - -',
-                  hintStyle: TextStyle(
-                    fontSize: 30,
+              // Otp input field
+              SizedBox(
+                width: size.width * 0.35,
+                child: TextField(
+                  cursorColor: textColor,
+                  textAlign: TextAlign.center,
+                  style: const TextStyle(fontSize: 20),
+                  keyboardType: TextInputType.number,
+                  decoration: const InputDecoration(
+                    hintText: '- - - - - -',
+                    hintStyle: TextStyle(
+                      fontSize: 30,
+                    ),
                   ),
+                  onChanged: (val) {
+                    if (val.length == 6) {
+                      verifyOtp(ref, context, val.trim());
+                    }
+                  },
                 ),
-                onChanged: (val) {
-                  if (val.length == 6) {
-                    verifyOtp(ref, context, val.trim());
-                  }
-                },
               ),
-            ),
-            const SizedBox(height: 16),
+              const SizedBox(height: 16),
 
-            const Text('Enter a 6-digit code'),
+              const Text('Enter a 6-digit code'),
 
-            // Resend code button
-            TextButton(
-              onPressed: () {},
-              child: Text(
-                'Resend code',
-                style: TextStyle(color: textColor),
+              // Resend code button
+              TextButton(
+                onPressed: () {},
+                child: Text(
+                  'Resend code',
+                  style: TextStyle(color: textColor),
+                ),
               ),
-            ),
-          ],
+            ],
+          ),
         ),
       ),
     );
--- lib/features/auth/screens/user_information_screen.dart
+++ lib/features/auth/screens/user_information_screen.dart
@@ -1,5 +1,9 @@
+import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:whatsapp_clone/data/colors.dart';
+import 'package:emoji_picker_flutter/emoji_picker_flutter.dart';
+import 'package:whatsapp_clone/shared_features/image_picker.dart';
+import 'package:whatsapp_clone/shared_features/custom_button.dart';
 import 'package:whatsapp_clone/shared_features/pic_modal_sheet.dart';
 
 class UserInfromationScreen extends StatefulWidget {
@@ -12,6 +16,8 @@ class UserInfromationScreen extends StatefulWidget {
 }
 
 class _UserInfromationScreenState extends State<UserInfromationScreen> {
+  File? _image;
+  var _showEmoji = false;
   final _nameController = TextEditingController();
 
   @override
@@ -20,7 +26,31 @@ class _UserInfromationScreenState extends State<UserInfromationScreen> {
     super.dispose();
   }
 
-  void _addImage() {
+  void _toggleEmoji() {
+    setState(() {
+      _showEmoji = !_showEmoji;
+    });
+  }
+
+//Gallery Image Picker
+  void _pickImageFromGallery() async {
+    _image = await pickImageFromGallery(context);
+    setState(() {});
+    if (mounted) {
+      Navigator.of(context).pop();
+    }
+  }
+
+//Camera Image Picker
+  void _pickImageFromCamera() async {
+    _image = await pickImageFromCamera(context);
+    setState(() {});
+    if (mounted) {
+      Navigator.of(context).pop();
+    }
+  }
+
+  void _bottomSheetStructure() {
     showModalBottomSheet(
       enableDrag: true,
       showDragHandle: true,
@@ -31,26 +61,31 @@ class _UserInfromationScreenState extends State<UserInfromationScreen> {
           padding: const EdgeInsets.all(20),
           child: ConstrainedBox(
             constraints: BoxConstraints(
-              maxHeight: MediaQuery.of(context).size.height - 700,
-            ),
-            child: const Column(
+                maxHeight: MediaQuery.of(context).size.height - 700),
+            child: Column(
               crossAxisAlignment: CrossAxisAlignment.start,
               children: [
-                Text(
+                const Text(
                   'Profile photo',
                   style: TextStyle(fontSize: 22, fontWeight: FontWeight.w500),
                 ),
-                SizedBox(height: 24),
+                const SizedBox(height: 24),
                 Row(
                   children: [
-                    CustomCircularAvator(
-                      icon: Icons.camera_alt,
-                      text: 'Camera',
+                    InkWell(
+                      onTap: _pickImageFromCamera,
+                      child: const CustomCircularAvator(
+                        text: 'Camera',
+                        icon: Icons.camera_alt,
+                      ),
                     ),
-                    SizedBox(width: 24),
-                    CustomCircularAvator(
-                      icon: Icons.photo,
-                      text: 'Gallery',
+                    const SizedBox(width: 24),
+                    InkWell(
+                      onTap: _pickImageFromGallery,
+                      child: const CustomCircularAvator(
+                        text: 'Gallery',
+                        icon: Icons.photo,
+                      ),
                     ),
                   ],
                 )
@@ -78,54 +113,89 @@ class _UserInfromationScreenState extends State<UserInfromationScreen> {
             child: Column(
               children: [
                 const Text(
-                    'Please provide your name and an optional profile photo.'),
+                  'Please provide your name and an optional profile photo.',
+                ),
                 const SizedBox(height: 24),
 
                 //Profile Image Picker
                 Stack(
                   children: [
-                    CircleAvatar(
-                      backgroundColor: lightMode
-                          ? const Color.fromRGBO(235, 232, 232, 1)
-                          : const Color.fromARGB(255, 55, 55, 55),
-                      radius: 80,
-                      child: const SizedBox(
-                        child: Icon(
-                          Icons.person,
-                          size: 120,
-                          color: Color.fromARGB(255, 137, 136, 136),
-                        ),
-                      ),
-                    ),
+                    _image == null
+                        ? CircleAvatar(
+                            backgroundColor: lightMode
+                                ? const Color.fromRGBO(235, 232, 232, 1)
+                                : const Color.fromARGB(255, 55, 55, 55),
+                            radius: 80,
+                            child: const SizedBox(
+                              child: Icon(
+                                Icons.person,
+                                size: 120,
+                                color: Color.fromARGB(255, 137, 136, 136),
+                              ),
+                            ),
+                          )
+                        : CircleAvatar(
+                            radius: 80,
+                            backgroundImage: FileImage(_image!),
+                          ),
                     Positioned(
                       right: 0,
                       bottom: 0,
                       child: IconButton(
-                        onPressed: _addImage,
+                        onPressed: _bottomSheetStructure,
                         icon: const Icon(Icons.add_a_photo, color: tabColor),
                       ),
                     ),
-
-                    //Name text field
                   ],
                 ),
                 const SizedBox(height: 24),
+
+                //Name text field
                 Row(
                   children: [
                     Expanded(
                       child: TextField(
+                        maxLength: 25,
+                        cursorColor: tabColor,
+                        enableSuggestions: true,
                         controller: _nameController,
                         decoration: const InputDecoration(
                           hintText: 'Type your name here',
                         ),
+                        onTap: () {
+                          setState(() {
+                            _showEmoji = false;
+                          });
+                        },
                       ),
                     ),
                     IconButton(
-                      onPressed: () {},
+                      onPressed: _toggleEmoji,
                       icon: const Icon(Icons.emoji_emotions_outlined),
-                    )
+                    ),
                   ],
-                )
+                ),
+                _showEmoji ? const SizedBox(height: 40) : const Spacer(),
+                Padding(
+                  padding: const EdgeInsets.symmetric(
+                    vertical: 8,
+                    horizontal: 145,
+                  ),
+                  child: CustomButton(text: 'Next', onPressed: () {}),
+                ),
+                _showEmoji
+                    ? Expanded(
+                        child: EmojiPicker(
+                          config: Config(
+                            columns: 8,
+                            indicatorColor: tabColor,
+                            iconColorSelected: tabColor,
+                            bgColor: lightMode ? whiteColor : backgroundColor,
+                          ),
+                          textEditingController: _nameController,
+                        ),
+                      )
+                    : const SizedBox(),
               ],
             ),
           ),
